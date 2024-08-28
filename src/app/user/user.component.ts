5d096a4c267a849f8286d37aaaf3e3b9c4f71161
Mohamed Ryan <reyoryan@gmail.com>
2024-08-28 22:44:52 +0400

    use Input decorator for component inputs, as it is used a lot in many projects

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,28 +8,15 @@ import { Component, computed, Input, input } from '@angular/core';
 })
 export class UserComponent {
   
-  // this Input decorator make the variable to settable from outside
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
+  // - this Input decorator make the variable to settable from outside
+  // - the '!' mark here telling the TS that we are aware that there is no assiging value for the variable
+  //   as we use it as an input so no need to assign value, but TS not understand it using for this.
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
-  // declare input for component using the input-signal approach
-  // this way get ride of putting the '!' mark as now the variable has a value.
-  // also in computed case, will be more effecient, as it will not been called unless the value changed, not like the getter.
-
-  // avatar = input<string>(); // avatar is optional
-  avatar = input.required<string>();
-  name = input.required<string>();
-
-  // using inputSignal approach over the Input decorator, 
-  // will give u the feature of using signals in overall,
-  // especially get use of its mechanism when working with states and detecting changes 
-  // and apply ui changes in more effecient way
-
-  imagePath = computed(() => 'assets/users/' + this.avatar() )
-
-  // get imagePath(){
-  //   return 'assets/users/' + this.avatar;
-  // }
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser(){ 
   }
