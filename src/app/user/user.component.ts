bf18b6613c1dfb48e6c0201cde75f50c91746292
Mohamed Ryan <reyoryan@gmail.com>
2024-08-28 22:39:42 +0400

    how to use signal-input instead of Input decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,14 +9,29 @@ import { Component, Input } from '@angular/core';
 export class UserComponent {
   
   // this Input decorator make the variable to settable from outside
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
 
-  get imagePath(){
-    return 'assets/users/' + this.avatar;
-  }
+  // declare input for component using the input-signal approach
+  // this way get ride of putting the '!' mark as now the variable has a value.
+  // also in computed case, will be more effecient, as it will not been called unless the value changed, not like the getter.
 
-  onSelectUser(){
+  // avatar = input<string>(); // avatar is optional
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  // using inputSignal approach over the Input decorator, 
+  // will give u the feature of using signals in overall,
+  // especially get use of its mechanism when working with states and detecting changes 
+  // and apply ui changes in more effecient way
+
+  imagePath = computed(() => 'assets/users/' + this.avatar() )
+
+  // get imagePath(){
+  //   return 'assets/users/' + this.avatar;
+  // }
+
+  onSelectUser(){ 
   }
 
 }
