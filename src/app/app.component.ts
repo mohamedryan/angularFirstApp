a033156ef9938749def7f0d327ab9dc59ba0f853
Mohamed Ryan <reyoryan@gmail.com>
2024-08-28 21:46:12 +0400

    use user component with inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy.users';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,7 @@ import { UserComponent } from "./user/user.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+
+}
