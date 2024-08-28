42d306563cd8f4b718a804a1fe9a35aecc7e6fdf
Mohamed Ryan <reyoryan@gmail.com>
2024-08-29 01:44:46 +0400

    use output decorator to make custom event, to emit data from the component to the parents

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -11,14 +11,20 @@ export class UserComponent {
   // - this Input decorator make the variable to settable from outside
   // - the '!' mark here telling the TS that we are aware that there is no assiging value for the variable
   //   as we use it as an input so no need to assign value, but TS not understand it using for this.
+  @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
+
+  // EventEmitter allow us to emit values outside to the parent ones.
+  @Output() select = new EventEmitter();
+
 
   get imagePath(){
     return 'assets/users/' + this.avatar;
   }
 
   onSelectUser(){ 
+    this.select.emit(this.id);
   }
 
 }
