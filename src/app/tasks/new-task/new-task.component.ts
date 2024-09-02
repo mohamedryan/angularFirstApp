8affcf59a76235e7b91a24199cc079dad172ebdb
Mohamed Ryan <reyoryan@gmail.com>
2024-09-03 01:56:47 +0400

    user service pattern to handle module data, also use dependency injection to inject services

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { type NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -11,26 +12,28 @@ import { type NewTaskData } from '../task/task.model';
 })
 export class NewTaskComponent {
   
-  @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter<NewTaskData>();
-
+  @Output() close = new EventEmitter<void>();
+  @Input({required: true}) userId!: string;
+  
   // you can use signal here -> enteredTitle = signal('')
   // and angular with ngModule will detect it is a signal and will read and write to it properly.
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
 
+  // another way to use injected service.
+  private tasksService = inject(TasksService);
 
 
-  onCancel() {
-    this.cancel.emit();
+  onClose() {
+    this.close.emit();
   }
 
   onSubmit(){
-    this.add.emit({
+    this.tasksService.addTask({
       title: this.enteredTitle,
       summary: this.enteredSummary,
       date: this.enteredDate
-    })
+    }, this.userId);
   }
 }
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, inject, Input, Output } from '@angular/core';
 import { type Task } from './task.model';
 import { CardComponent } from "../../shared/card/card.component";
 import { DatePipe } from '@angular/common';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-task',
@@ -12,12 +13,12 @@ import { DatePipe } from '@angular/common';
 })
 export class TaskComponent {
   
-  @Input({required: true}) task!: Task; 
-  @Output() complete = new EventEmitter<string>();
+  @Input({required: true}) task!: Task;
 
+  private tasksService = inject(TasksService);
 
   onCompleteTask(){
-    this.complete.emit(this.task.id);
+    this.tasksService.deleteTask(this.task.id);
   }
 
 }
