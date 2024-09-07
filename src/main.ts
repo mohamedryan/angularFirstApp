612c668de518a2910a9aee67c2fa72db19220d3d
Mohamed Ryan <reyoryan@gmail.com>
2024-09-07 23:04:01 +0400

    migrate standalone to module-based components using ngModule

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+// import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppComponent } from './app/app.component';
+// import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+// // works only with standalone component
+// bootstrapApplication(AppComponent).catch((err) => console.error(err));
+
+
+// this approach to start angular app that uses modules
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app/app.module";
+
+platformBrowserDynamic().bootstrapModule(AppModule)
